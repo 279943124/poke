754e53702b3bf3395cd8957d50f975c7b6ca228b
279943124 <279943124@qq.com>
2020-08-20 20:10:14 +0800

    初始化项目

---
+++ Poke/Poke.Con/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Poke.Con
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 新建服务端的socket
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+        }
+    }
+}
