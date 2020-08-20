78c6fe245e33e009421879e488969ffe510f57f4
279943124 <279943124@qq.com>
2020-08-20 20:25:54 +0800

    初始化socket代码

---
+++ Poke/Poke.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace Poke.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 10056));
+            socket.Send(System.Text.Encoding.Default.GetBytes("大肥猪"));
+            byte[] buffer = new byte[1024];
+            int len = socket.Receive(buffer);
+            Console.WriteLine(System.Text.Encoding.Default.GetString(buffer,0,len));
+            Console.ReadLine();
+        }
+    }
+}
--- Poke/Poke.Con/Program.cs
+++ Poke/Poke.Con/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
+using System.Net;
 using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Poke.Con
 {
     class Program
     {
+        private static Socket serverSocket;
         static void Main(string[] args)
         {
             // 新建服务端的socket
-            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 10056);
+            serverSocket.Bind(endPoint);
+            serverSocket.Listen(10);
+            //TaskFactory taskFactory = new TaskFactory();
+            //// 开启一个线程接受客户端连接
+            //taskFactory.StartNew(ListenClientConnect);
+            Thread thread = new Thread(ListenClientConnect);
+            thread.IsBackground = false;
+            thread.Start();
+        }
+        private static void ListenClientConnect()
+        {
+            Socket clientSocket = serverSocket.Accept();
+            clientSocket.Send(System.Text.Encoding.Default.GetBytes("Hello World"));
+            Thread thread = new Thread(ReceiveClientMessage);
+            thread.Start(clientSocket);
+        }
+        private static void ReceiveClientMessage(object clientSocket)
+        {
+            Socket client = (Socket)clientSocket;
+            byte[] buffer = new byte[1024];
+            int len = client.Receive(buffer);
+            Console.WriteLine(System.Text.Encoding.Default.GetString(buffer,0,len));
         }
     }
 }
